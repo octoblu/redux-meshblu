45b50f5845cd4d1493f8f6603778244484ba60c8
kmariano / oayandosu <koshin.mariano@gmail.com , oayandosu@gmail.com>
2016-11-08 15:19:06 -0700

    v4.3.1 update and updateDangerously actions were missing from export

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import getDevice, * as getDeviceActions from './Actions/DevicesGet/'
 import listSubscriptions, * as listSubscriptionsActions from './Actions/SubscriptionsList/'
 import search, * as searchActions from './Actions/Search/'
 import unregister, * as unregisterActions from './Actions/Unregister/'
+import update, * as updateActions from './Actions/Update/'
+import updateDangerously, * as updateDangerouslyActions from './Actions/UpdateDangerously/'
 
 export {
   getDevice,
@@ -12,4 +14,8 @@ export {
   searchActions,
   unregister,
   unregisterActions,
+  update,
+  updateActions,
+  updateDangerously,
+  updateDangerouslyActions,
 }
